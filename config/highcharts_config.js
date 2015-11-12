333f1cbca72878d970d2c0e1d7321d14d99aa8fb
simonefenix <simone.murzilli@fao.org>
2015-11-12 20:25:03 +0100

    .

--- config/highcharts_config.js
+++ config/highcharts_config.js
@@ -4,119 +4,278 @@ define([], function () {
     'use strict';
 
     return {
-        chart: {
-            type: 'pie',
-            alignTicks: false,
-            backgroundColor: '#FFFFFF',
-            spacing: [20, 1, 1, 1],
-            plotBorderColor: '#ffffff',
-            plotBorderWidth: 0,
-            style: {
-                fontFamily: 'Roboto',
-                fontSize: '12px',
-                fontWeight: 300
-            },
-            zoomType: 'xy',
-            resetZoomButton: {
-                position: {
-                    align: 'right',
-                    x: -10
+
+        'pie': {
+            chart: {
+                type: 'pie',
+                alignTicks: false,
+                backgroundColor: '#FFFFFF',
+                spacing: [20, 1, 1, 1],
+                plotBorderColor: '#ffffff',
+                plotBorderWidth: 0,
+                style: {
+                    fontFamily: 'Roboto',
+                    fontSize: '12px',
+                    fontWeight: 300
                 },
-                theme: {
-                    fill: '#FFFFFF',
-                    stroke: '#666666',
-                    width: 60,
-                    style: {
-                        textAlign: 'center',
-                        fontSize: 10
+                zoomType: 'xy',
+                resetZoomButton: {
+                    position: {
+                        align: 'right',
+                        x: -10
                     },
-                    states: {
-                        hover: {
-                            fill: '#e6e6e6',
-                            stroke: '#666666'
+                    theme: {
+                        fill: '#FFFFFF',
+                        stroke: '#666666',
+                        width: 60,
+                        style: {
+                            textAlign: 'center',
+                            fontSize: 10
+                        },
+                        states: {
+                            hover: {
+                                fill: '#e6e6e6',
+                                stroke: '#666666'
+                            }
+                        }
+                    }
+                }
+            },
+            credits: {
+                enabled: false
+            }
+            ,
+            exporting: {
+                enabled: true
+            }
+            ,
+            navigation: {
+                buttonOptions: {
+                    theme: {
+                        'stroke-width': 1,
+                        stroke: '#666666',
+                        r: 0,
+                        states: {
+                            hover: {
+                                stroke: '#666666',
+                                fill: '#e6e6e6'
+                            }
+                            ,
+                            select: {
+                                stroke: '#666666',
+                                fill: '#e6e6e6'
+                            }
                         }
                     }
                 }
             }
+            ,
+            legend: {
+                align: 'right',
+                verticalAlign: 'middle',
+                layout: 'vertical',
+
+                itemMarginTop: 5,
+                itemMarginBottom: 5,
+                itemStyle: {
+                    cursor: 'pointer',
+                    color: '#666666',
+                    fontSize: '11px',
+                    fontWeight: 300
+                }
+                ,
+                itemWidth: 150,
+                itemHiddenStyle: {
+                    color: '#eeeeee'
+                }
+                ,
+                itemHoverStyle: {
+                    color: '#3ca7da'
+                }
+            }
+            ,
+            plotOptions: {
+                pie: {
+                    borderWidth: 1,
+                    startAngle: -45,
+                    dataLabels: {
+                        enabled: false,
+                        softConnector: false
+                    }
+                }
+            }
+            ,
+            title: {
+                enabled: true,
+                text: null,
+                x: -20
+            }
+            ,
+            subtitle: {
+                text: null,
+                x: -20
+            }
+            ,
+            tooltip: {
+                shadow: true
+            }
+
         },
-        credits: {
-            enabled: false
-        }
-        ,
-        exporting: {
-            enabled: true
-        }
-        ,
-        navigation: {
-            buttonOptions: {
-                theme: {
-                    'stroke-width': 1,
-                    stroke: '#666666',
-                    r: 0,
-                    states: {
-                        hover: {
-                            stroke: '#666666',
-                            fill: '#e6e6e6'
+        'timeseries': {
+            chart: {
+                alignTicks: false,
+                backgroundColor: '#FFFFFF',
+                spacing: [20, 1, 1, 1],
+                plotBorderColor: '#ffffff',
+                plotBorderWidth: 0,
+                style: {
+                    fontFamily: 'Roboto',
+                    fontSize: '12px',
+                    fontWeight: 300
+                },
+                zoomType: 'xy',
+                resetZoomButton: {
+                    position: {
+                        align: 'right',
+                        x: -10
+                    },
+                    theme: {
+                        fill: '#FFFFFF',
+                        stroke: '#666666',
+                        width: 60,
+                        style: {
+                            textAlign: 'center',
+                            fontSize: 10
+                        },
+                        states: {
+                            hover: {
+                                fill: '#e6e6e6',
+                                stroke: '#666666'
+                            }
                         }
-                        ,
-                        select: {
-                            stroke: '#666666',
-                            fill: '#e6e6e6'
+                    }
+
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            exporting: {
+                enabled: true
+            },
+            navigation: {
+                buttonOptions: {
+                    theme: {
+                        'stroke-width': 1,
+                        stroke: '#666666',
+                        r: 0,
+                        states: {
+                            hover: {
+                                stroke: '#666666',
+                                fill: '#e6e6e6'
+                            },
+                            select: {
+                                stroke: '#666666',
+                                fill: '#e6e6e6'
+                            }
                         }
                     }
                 }
-            }
-        }
-        ,
-        legend: {
-            align: 'right',
-            verticalAlign: 'middle',
-            layout: 'vertical',
-
-            itemMarginTop: 5,
-            itemMarginBottom: 5,
-            itemStyle: {
-                cursor: 'pointer',
-                color: '#666666',
-                fontSize: '11px',
-                fontWeight: 300
-            }
-            ,
-            itemWidth: 150,
-            itemHiddenStyle: {
-                color: '#eeeeee'
-            }
-            ,
-            itemHoverStyle: {
-                color: '#3ca7da'
-            }
-        }
-        ,
-        plotOptions: {
-            pie: {
-                borderWidth: 1,
-                startAngle: -45,
-                dataLabels: {
-                    enabled: false,
-                    softConnector: false
+            },
+            legend: {
+                enabled: true,
+                floating: false,
+                backgroundColor: '#FFFFFF',
+                align: 'center',
+                verticalAlign: 'bottom',
+                borderWidth: 0,
+                symbolPadding: 10,
+                itemStyle: {
+                    cursor: 'pointer',
+                    color: '#666666',
+                    fontSize: '11px',
+                    fontWeight: 300
+                },
+                itemHiddenStyle: {
+                    color: '#eeeeee'
+                },
+                itemHoverStyle: {
+                    color: '#3ca7da'
+                },
+                navigation: {
+                    activeColor: '#3ca7da',
+                    inactiveColor: '#666666',
+                    arrowSize: 8,
+                    animation: true,
+                    style: {
+                        color: '#666666',
+                        fontSize: '10px'
+                    }
                 }
-            }
-        }
-        ,
-        title: {
-            enabled: true,
-            text: null,
-            x: -20
-        }
-        ,
-        subtitle: {
-            text: null,
-            x: -20
-        }
-        ,
-        tooltip: {
-            shadow: true
-        }
+            },
+            plotOptions: {
+                series: {
+                    allowPointSelect: true,
+                    pointPlacement: "on",
+                    animation: {
+                        duration: 1000,
+                        easing: 'swing'
+                    },
+                    connectNulls: true,
+                    cropThreshold: 3,
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    fillColor: {
+                        linearGradient: [0, 0, 0, 350],
+                        stops: [
+                            [0, 'rgba(55, 155, 205,0.5)'],
+                            [1, 'rgba(255,255,255,0)']
+                        ]
+                    }
 
-    };
+                }
+            },
+            title: {
+                enabled: false,
+                text: null,
+                x: -20
+            },
+            subtitle: {
+                text: null,
+                x: -20
+            },
+            xAxis: {
+                gridLineWidth: 1,
+                lineColor: '#e0e0e0',
+                tickColor: '#e0e0e0',
+                gridLineColor: '#eeeeee',
+                minTickInterval: 5,
+                tickmarkPlacement: 'on',
+                labels: {
+                    y: 25,
+                    style: {
+                        color: '#666666',
+                        fontWeight: '300',
+                        fontSize: 12
+                    }
+                }
+            },
+            tooltip: {
+                backgroundColor: 'rgba(255, 255, 255, 0.95)',
+                borderWidth: 1,
+                shadow: false,
+                shared : true,
+                crosshairs: true,
+                valueDecimals: 2,
+                valuePrefix: '',
+                valueSuffix: ''
+            },
+        }
+    }
+
+
 });
--- libs/f3-ghg-chart.js
+++ libs/f3-ghg-chart.js
@@ -24,24 +24,16 @@ define([
 
         function createPie(obj, data) {
 
-            var colors = obj.colors || FENIX_COLORS;
-
-            var categories = [];
-            for (var i = 0; i < data; i++) {
-                categories.push(data[i][0]);
-            }
-
-            var series = [],
+            var colors = obj.colors || FENIX_COLORS,
+                series = [],
                 serie = {};
+
             serie.name = obj.serie.name;
             serie.type = 'pie';
             serie.data = [];
             serie.showInLegend = true;
             for (var i = 0; i < data.length; i++) {
-                var tmp = [];
-                tmp[0] = data[i][0];
-                tmp[1] = parseFloat(parseFloat(data[i][1]).toFixed(2));
-                serie.data.push(tmp);
+                serie.data.push([data[i][0], parseFloat(parseFloat(data[i][1]).toFixed(2))]);
             }
             series.push(serie);
 
@@ -53,7 +45,8 @@ define([
                 series: series
             };
 
-            var c = new Highcharts.Chart($.extend(true, {}, HighchartsConfig, chart));
+            var c = new Highcharts.Chart($.extend(true, {}, HighchartsConfig.pie, chart));
+
         }
 
         function createTimeserie(chart, type, s) {
@@ -126,7 +119,6 @@ define([
                 var yearsList = [];
                 for (var key in vectors) {
                     for (var i = 0; i < vectors[key].dates.length; i++) {
-                        // if the year still is not in the hashmap, add it
                         if (y.get(vectors[key].dates[i]) == null) {
                             y.put(vectors[key].dates[i], vectors[key].dates[i]);
                             yearsList.push(parseInt(vectors[key].dates[i]));
@@ -212,169 +204,20 @@ define([
                 }
             }
 
-            var chartRenderer = {
+            var chart = {
                 chart: {
                     type: type,
-                    renderTo: chart.renderTo,
-                    alignTicks: false,
-                    backgroundColor: '#FFFFFF',
-                    spacing: [20, 1, 1, 1],
-                    plotBorderColor: '#ffffff',
-                    plotBorderWidth: 0,
-                    style: {
-                        fontFamily: 'Roboto',
-                        fontSize: '12px',
-                        fontWeight: 300
-                    },
-                    zoomType: 'xy',
-                    resetZoomButton: {
-                        position: {
-                            align: 'right',
-                            x: -10
-                        },
-                        theme: {
-                            fill: '#FFFFFF',
-                            stroke: '#666666',
-                            width: 60,
-                            style: {
-                                textAlign: 'center',
-                                fontSize: 10
-                            },
-                            states: {
-                                hover: {
-                                    fill: '#e6e6e6',
-                                    stroke: '#666666'
-                                }
-                            }
-                        }
-
-                    }
+                    renderTo: chart.renderTo
                 },
                 colors: colors,
-                credits: {
-                    enabled: false
-                },
-                exporting: {
-                    enabled: true
-                },
-                navigation: {
-                    buttonOptions: {
-                        theme: {
-                            'stroke-width': 1,
-                            stroke: '#666666',
-                            r: 0,
-                            states: {
-                                hover: {
-                                    stroke: '#666666',
-                                    fill: '#e6e6e6'
-                                },
-                                select: {
-                                    stroke: '#666666',
-                                    fill: '#e6e6e6'
-                                }
-                            }
-                        }
-                    }
-                },
-                legend: {
-                    enabled: true,
-                    floating: false,
-                    backgroundColor: '#FFFFFF',
-                    align: 'center',
-                    verticalAlign: 'bottom',
-                    borderWidth: 0,
-                    symbolPadding: 10,
-                    itemStyle: {
-                        cursor: 'pointer',
-                        color: '#666666',
-                        fontSize: '11px',
-                        fontWeight: 300
-                    },
-                    itemHiddenStyle: {
-                        color: '#eeeeee'
-                    },
-                    itemHoverStyle: {
-                        color: '#3ca7da'
-                    },
-                    navigation: {
-                        activeColor: '#3ca7da',
-                        inactiveColor: '#666666',
-                        arrowSize: 8,
-                        animation: true,
-                        style: {
-                            color: '#666666',
-                            fontSize: '10px'
-                        }
-                    }
-                },
-                plotOptions: {
-                    series: {
-                        allowPointSelect: true,
-                        pointPlacement: "on",
-                        animation: {
-                            duration: 1000,
-                            easing: 'swing'
-                        },
-                        connectNulls: true,
-                        cropThreshold: 3,
-                        lineWidth: 1,
-                        states: {
-                            hover: {
-                                lineWidth: 1
-                            }
-                        },
-                        fillColor: {
-                            linearGradient: [0, 0, 0, 350],
-                            stops: [
-                                [0, 'rgba(55, 155, 205,0.5)'],
-                                [1, 'rgba(255,255,255,0)']
-                            ]
-                        }
-
-                    }
-                },
-
-                title: {
-                    enabled: false,
-                    text: null,
-                    x: -20
-                },
-                subtitle: {
-                    text: null,
-                    x: -20
-                },
                 xAxis: {
-                    categories: years,
-                    gridLineWidth: 1,
-                    lineColor: '#e0e0e0',
-                    tickColor: '#e0e0e0',
-                    gridLineColor: '#eeeeee',
-                    minTickInterval: 5,
-                    tickmarkPlacement: 'on',
-                    labels: {
-                        y: 25,
-                        style: {
-                            color: '#666666',
-                            fontWeight: '300',
-                            fontSize: 12
-                        }
-                    }
+                    categories: years
                 },
                 yAxis: yAxis,
-                tooltip: {
-                    backgroundColor: 'rgba(255, 255, 255, 0.95)',
-                    borderWidth: 1,
-                    shadow: false,
-                    shared : true,
-                    crosshairs: true,
-                    valueDecimals: 2,
-                    valuePrefix: '',
-                    valueSuffix: ''
-                },
                 series: series
             }
 
-            var c = new Highcharts.Chart(chartRenderer);
+            var c = new Highcharts.Chart($.extend(true, {}, HighchartsConfig.timeseries, chart));
         }
 
         return {
